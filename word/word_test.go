7ba5670b2c50ebaf7ae445248d37225dd43a4710
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-27 15:40:46 -0500

    First tests

---
+++ word/word_test.go
@@ -0,0 +1,31 @@
+package word
+
+import "testing"
+
+func TestIsPalindrome(t *testing.T) {
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak") {
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome") {
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
+
+func TestAccentedPalindrome(t *testing.T) {
+	if !IsPalindrome("eté") {
+		t.Error(`IsPalindrome("eté") = false`)
+	}
+}
+
+func TestPalindromeWithPunctuation(t *testing.T){
+	input_words := "A man, a plan, a canal: Panamá"
+	if !IsPalindrome(input_words) {
+		t.Errorf(`IsPalindrome(%q) = false`, input_words)
+	}
+}
