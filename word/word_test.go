2ef8af4d4707d3fa285d33bc22d1e8b95f3474d0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-28 00:12:20 -0500

    Randomized test

--- word/word_test.go
+++ word/word_test.go
@@ -1,6 +1,10 @@
 package word
 
-import "testing"
+import (
+	"testing"
+	"math/rand"
+	"time"
+)
 
 func TestIsPalindrome(t *testing.T) {
 	if !IsPalindrome("detartrated") {
@@ -19,13 +23,56 @@ func TestNonPalindrome(t *testing.T) {
 
 func TestAccentedPalindrome(t *testing.T) {
 	if !IsPalindrome("été") {
-		t.Error(`IsPalindrome("eté") = false`)
+		t.Error(`IsPalindrome("été") = false`)
 	}
 }
 
 func TestPalindromeWithPunctuation(t *testing.T){
-	input_words := "A man, a plan, a canal: Panamá"
+	input_words := "A man, a plan, a canal: Panama"
 	if !IsPalindrome(input_words) {
 		t.Errorf(`IsPalindrome(%q) = false`, input_words)
+	}
+}
+
+func TestSeveralExamples(t *testing.T){
+	var tests = []struct {
+		input string
+		result bool
+	} {
+		{"", true},
+		{"a", true},
+		{"aaa", true},
+		{"aab", false}, // no es un palíndromo
+		{"Anita lava la tina", true},
+		{"yes", false}, // No es un palíndromo
+	}
+	for _, test := range tests {
+		if !IsPalindrome(test.input) == test.result {
+			t.Errorf(`Error IsPalindrome(%q) == %v`, test.input, test.result)
+		}
+	}
+}
+
+func getRandomPalindrome(rng *rand.Rand) string{
+	n := rng.Intn(25) // Número aleatóreo hasta 24,
+	runes := make([]rune, n)
+	for i := 0; i < (n + 1) / 2 ; i++ {
+		r :=  rune(rng.Intn(0x1000))
+		runes[i] = r
+		runes[n - 1 - i]= r
+	}
+	return string(runes)
+}
+
+func TestRandomPalindromes(t *testing.T) {
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+	var palindrome string
+	for i := 0; i < 1000; i++ {
+		palindrome = getRandomPalindrome(rng)
+		if IsPalindrome(palindrome) {
+			t.Errorf(`IsPalindrome failed with random palindrome %q`, palindrome)
+		}
 	}
 }
