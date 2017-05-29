5093eb351a600f2568b362de04d914a00eeacdb9
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-05-29 00:01:14 -0500

    Función Main para echo

--- command/echo.go
+++ command/echo.go
@@ -11,3 +11,13 @@ var (
 	n = flag.Bool("n", false, "omitiendo nueva línea al final")
 	s = flag.String("s", " ", "Separador")
 )
+
+var out io.Writer = os.Stdout
+
+func main(){
+	flag.Parse()
+	if err := echo(!*n, *s, flag.Args()); err != nil {
+		fmt.Fprintf(os.Stderr, "echo %v\n", err)
+		os.Exit(1)
+	}
+}
